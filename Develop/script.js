8718f9d9755d6c4308d027dc5e3604070c6d6aa7
Matt Thurber <mthurber@Matts-MacBook-Pro-2.local>
2021-09-29 13:01:53 -0600

    attampting to fix syntax error and get successful password generation

--- Develop/script.js
+++ Develop/script.js
@@ -6,24 +6,22 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var length = 8,
-  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
   var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var symbols = "!@#$%^&*_-+=";
-  password = "";
+  
 for (var i = 0, n = charset.length; i < length; ++i) {
   password += charset.charAt(Math.floor(Math.random() * n));
-}
+
 return password;
-
 };
-
 // Display Password
 document.getElementById("displaypassword").value = (password);
 
 };
 
-// Add event listener to generate button
+// Add event listener to generate button, cherck for passsword requirements
 generateBtn.addEventListener("click", () => {
   let characters = alpha;
   incNumbers.checked ? (characters += numbers) : "";
