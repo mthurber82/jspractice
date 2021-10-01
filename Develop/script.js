4ca27c9d71a89a2915c36b2d238067294faa186a
Matt Thurber <mthurber@Matts-MacBook-Pro-2.local>
2021-09-30 18:40:16 -0600

    addessing variable definitions and code errors

--- Develop/script.js
+++ Develop/script.js
@@ -3,28 +3,47 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var length = 8,
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-  var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var symbols = "!@#$%^&*_-+=";
-  
-for (var i = 0, n = charset.length; i < length; ++i) {
-  password += charset.charAt(Math.floor(Math.random() * n));
+  var pwlength = prompt("How long would you like your password to be?");
+  pwlength = parseInt(pwlength);
+  var isLower = confirm("Do you want lowercase characters included?");
+  var isUpper = confirm("Do you want uppercase characters included?");
+  var isNumbers = confirm("Do you want numbers included");
+  var isSymbols = confirm("Do you want special characters included?");
+  var characterChoices = "";
+  var generatedPasword = "";
 
-return password;
-};
-// Display Password
-document.getElementById("displaypassword").value = (password);
+  if (isLower === true) {
+    characterChoices = characterChoices + lower
+  }
+
+  if (isUpper === true) {
+    characterChoices = characterChoices + upper
+  }
+
+  if (isNumbers === true) {
+    characterChoices = characterChoices + numbers
+  }
+
+  if (isSymbols === true) {
+    characterChoices = characterChoices + symbols
+  }
+
+console.log(characterChoices);
+
+  for (var i = 0; i < pwlength; i++) {
+    var randomChar = characterChoices.charAt(Math.floor(Math.random() * characterChoices.length));
+    generatedPasword = generatedPasword + randomChar;
+    console.log(generatedPasword);
+    return generatedPasword;
+  };
 
 };
 
 // Add event listener to generate button, cherck for passsword requirements
-generateBtn.addEventListener("click", () => {
-  let characters = alpha;
-  incNumbers.checked ? (characters += numbers) : "";
-  incSymbols.checked ? (characters += symbols) : "";
-  passwordTxt.value = generatePassword(length.value, characters);
-});
+generateBtn.addEventListener("click", writePassword);
